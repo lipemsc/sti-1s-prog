f83ebb1b02ccc2e4f72889a7f3210c56653ec2b0
Filipe <lipe.msc@gmail.com>
2022-01-20 14:56:51 +0000

    Ex 3 from 4th class sucessfully done

--- aula4/ex3.py
+++ aula4/ex3.py
@@ -3,4 +3,9 @@ nums = list()
 for i in range(int(input("Insira o tamanho da lista: "))):
     nums.append(int(input("Insira o {0} valor: ".format(i + 1))))
 
-nums.index()
+try:
+    nums.index(int(input("Introduza o valor a procurar: ")))
+except ValueError:
+    print("Não encontrei")
+else:
+    print("Encontrei")
